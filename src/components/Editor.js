8ddaa354aa96236998b9dd11edd4fd22f0268848
Cooper Viktor <im.cooperviktor@gmail.com>
2020-12-06 14:15:08 +1100

    done

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,78 @@
-import React from "react";
+import { useState } from "react";
+import { CirclePicker } from "react-color";
+import DrawingPanel from "./DrawingPanel";
+import "../styles/editor.scss";
 
 const Editor = () => {
+	const [panelWidth, setPanelWidth] = useState(16);
+	const [panelHeight, setPanelHeight] = useState(16);
+	const [hideOptions, setHideOptions] = useState(false);
+	const [hideDrawingPanel, setHideDrawingPanel] = useState(true);
+	const [buttonText, setButtonText] = useState("start drawing");
+	const [selectedColor, setColor] = useState("#f44336");
+
+	function initializeDrawingPanel() {
+		setHideOptions(!hideOptions);
+		setHideDrawingPanel(!hideDrawingPanel);
+
+		buttonText === "start drawing"
+			? setButtonText("reset")
+			: setButtonText("start drawing");
+	}
+
+	function changeColor(color) {
+		setColor(color.hex);
+	}
+
 	return (
-		<div>
-			<h1>Editor yo</h1>
+		<div id="editor">
+			<h1>Pixel Editor</h1>
+			{hideDrawingPanel && <h2>Enter Panel Dimensions</h2>}
+			{hideDrawingPanel && (
+				<div id="options">
+					<div className="option">
+						<input
+							type="number"
+							className="panelInput"
+							defaultValue={panelWidth}
+							onChange={(e) => {
+								setPanelWidth(e.target.value);
+							}}
+						/>
+						<span>Width</span>
+					</div>
+					<div className="option">
+						<input
+							type="number"
+							className="panelInput"
+							defaultValue={panelHeight}
+							onChange={(e) => {
+								setPanelHeight(e.target.value);
+							}}
+						/>
+						<span>Height</span>
+					</div>
+				</div>
+			)}
+
+			<button onClick={initializeDrawingPanel} className="button">
+				{buttonText}
+			</button>
+
+			{hideOptions && (
+				<CirclePicker
+					color={selectedColor}
+					onChangeComplete={changeColor}
+				/>
+			)}
+
+			{hideOptions && (
+				<DrawingPanel
+					width={panelWidth}
+					height={panelHeight}
+					selectedColor={selectedColor}
+				/>
+			)}
 		</div>
 	);
 };
